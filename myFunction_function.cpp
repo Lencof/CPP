d556603dbe531fecff38f990fed8a7d7c2eb1280
Lencof <71154457+Lencof@users.noreply.github.com>
2021-08-22 08:05:08 +0300

    Update myFunction_function.cpp

--- myFunction_function.cpp
+++ myFunction_function.cpp
@@ -1,2 +1,14 @@
 // __Author__ __Lencof__
 // myFunction_function.cpp
+
+#include <iostream>
+using namespace std;
+
+void myFunction() {
+  cout << "Lico!";
+}
+
+int main() {
+  myFunction();
+  return 0;
+}
